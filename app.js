649fc1b5a4fd62b8f1fdfd6376849c2e18957d37
Amaury Gimonnet <amaurygimonnet@mac-1.home>
2026-05-16 19:53:35 +0200

    MongoDB

--- app.js
+++ app.js
@@ -2,6 +2,19 @@ const express = require('express'); // On installe le package express
 
 const app = express(); // On crée l'application express
 
+require('dotenv').config();
+
+const mongoose = require('mongoose'); // facilite les interactions avec notre base de données MongoDB
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch((error) => {
+  console.log('Connexion à MongoDB échouée !');
+  console.error(error.message);
+});
+
+app.use(express.json()); // Intercepte toutes les requêtes qui ont un content type en json et les met dans l'objet request
+
 app.use((req, res, next) => { // middleware pour accepter le Cross Origin Ressource Sharing (CORS)
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -9,8 +22,15 @@ app.use((req, res, next) => { // middleware pour accepter le Cross Origin Ressou
     next();
 });
 
+// route POST
+app.post('/api/stuff', (req, res, next) => {
+    console.log(req.body);
+    res.status(201).json({ Message: 'Objet créé'}); // le statut 201 signifie le succès de création
+    next();
+})
+
 // route GET
-app.use('/api/stuff', (req, res, next) => { // middleware pour recevoir une réponse à partir d'une route GET (on met en paramètres la route en plus du reste)
+app.get('/api/stuff', (req, res, next) => { // middleware pour recevoir une réponse à partir d'une route GET (on met en paramètres la route en plus du reste)
     const stuff = [
         {
             _id: 1,
