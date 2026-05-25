2a824e49b4b6963815926aad5a5aa80b4fc76701
Amaury Gimonnet <amaurygimonnet@Amaurys-MacBook-Pro.local>
2026-05-25 13:08:03 +0200

    Authentification

--- routes/stuff.js
+++ routes/stuff.js
@@ -1,21 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+const auth = require('../middleware/auth');
+
 const stuffCtrl = require('../controllers/stuff');
 
 // route POST
-router.post('/', stuffCtrl.createThing);
+router.post('/', auth, stuffCtrl.createThing);
 
 // route PUT (modification d'un Thing spécifique)
-router.put('/:id', stuffCtrl.modifyThing);
+router.put('/:id', auth, stuffCtrl.modifyThing);
 
 // route DELETE
-router.delete('/:id', stuffCtrl.deleteThing);
+router.delete('/:id', auth, stuffCtrl.deleteThing);
 
 // route GET pour un Thing spécifique
-router.get('/:id', stuffCtrl.getOneThing);
+router.get('/:id', auth, stuffCtrl.getOneThing);
 
 // route GET pour tous les Things
-router.get('/', stuffCtrl.getAllThings);
+router.get('/', auth, stuffCtrl.getAllThings);
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,8 @@
 const express = require('express');
-
 const router = express.Router();
+const userCtrl = require('../controllers/user');
+
+router.post('/signup', userCtrl.signup); 
+router.post('/login', userCtrl.login); // Le frontend poste les mail et mot de passe
 
 module.exports = router;
